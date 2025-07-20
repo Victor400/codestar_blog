8e0dec49a9d9859ebec7dd7fe684f99dd44c4af8
Olu Adesina <olu-adesina@hotmail.com>
2025-07-20 23:34:50 +0100

    Updated urls.py to include blog app routes and connected blog.views.my_blog

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import my_blog
+
+urlpatterns = [
+    path('', my_blog, name='my_blog'),
+]
--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def my_blog(request):
-    return HttpResponse("Hello, blog!")
+    return HttpResponse("Welcome to the blog!")
