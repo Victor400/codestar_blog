4d343b49b89afeca0bc86f6174e130557164a5d5
Olu Adesina <olu-adesina@hotmail.com>
2025-07-21 19:21:28 +0100

    Enable WhiteNoise for static files

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
+# blog/urls.py
 from django.urls import path
-from .views import my_blog
+from . import views
 
 urlpatterns = [
-    path('', my_blog, name='my_blog'),
+    path('', views.home, name='home'),  # This maps the root of /blog/
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
-def my_blog(request):
-    return HttpResponse("Welcome to the blog!")
+def home(request):
+    return HttpResponse("Welcome to the Blog Homepage!")
