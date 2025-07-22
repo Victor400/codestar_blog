dbfe81bf3aca1fe08f1f36d5b7180e4fbc3fea03
Olu Adesina <olu-adesina@hotmail.com>
2025-07-22 21:29:09 +0100

    delete env.py

--- codestar/env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_O6PZX9VnjubW@ep-royal-unit-a2g4tkzs.eu-central-1.aws.neon.tech/front_sport_dose_837195")
-os.environ.setdefault("SECRET_KEY", "a%=4v@6ow^%i-2d2t&37qtw5-(oepflf6i$e-f&k-2^_3%xmt1")
-
