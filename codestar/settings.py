7aaf4c03a9806568499577938352f79cb22f88e4
Olu Adesina <olu-adesina@hotmail.com>
2025-07-23 23:21:57 +0100

    Cleaned staticfiles and fixed manifest issues

--- codestar/settings.py
+++ codestar/settings.py
@@ -117,6 +117,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
