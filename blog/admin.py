b47daac279011da856d20780fe681f3ca284d06a
Olu Adesina <olu-adesina@hotmail.com>
2025-07-30 13:17:12 +0100

    chore: add all commited changes

--- blog/admin.py
+++ blog/admin.py
@@ -2,14 +2,16 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin 
 
+from django.utils.timezone import localtime
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
+    list_display = ('title', 'slug', 'status', 'created_on_display')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
-#admin.site.register(Post, PostAdmin)  # ✅ linked properly
-admin.site.register(Comment)
+    def created_on_display(self, obj):
+        return localtime(obj.created_on).strftime('%Y-%m-%d %H:%M')
+    created_on_display.short_description = 'Created On'
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.views import generic
 from .models import Post
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)  # Only show published posts
+    queryset = Post.objects.all()  # Only show published posts
     template_name = "blog/post_list.html"
     context_object_name = "posts"  # <== This must match your template loop
+    
     
