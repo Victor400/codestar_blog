4f24e9e977ab70f2cc65fd186e47750fc7166117
Olu Adesina <olu-adesina@hotmail.com>
2025-08-01 00:43:28 +0100

    git commit -m added post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -7,5 +8,21 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
     context_object_name = "posts"  # <== This must match your template loop
+
+    from django.shortcuts import render, get_object_or_404  # ✅ Step 6
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
+
     
     
