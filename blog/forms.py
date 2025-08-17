d617e7435bd2ad49feb4dd25dadbfcc482288702
Olu Adesina <olu-adesina@hotmail.com>
2025-08-17 15:20:05 +0100

    Implement comment CRUD features with validation and flash messaging

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,37 @@
 from django import forms
 from .models import Comment
 
+
 class CommentForm(forms.ModelForm):
+    """Form for adding/editing comments."""
+
     class Meta:
         model = Comment
-        fields = ('body',)
+        fields = ("body",)
+        widgets = {
+            "body": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Write your comment here...",
+                    "rows": 4,
+                }
+            ),
+        }
+        labels = {
+            "body": "",
+        }
+
+    def clean_body(self):
+        """Validate the comment body."""
+        body = self.cleaned_data.get("body", "").strip()
+
+        if not body:
+            raise forms.ValidationError("Comment cannot be empty.")
+
+        if len(body) < 5:
+            raise forms.ValidationError("comment, at least 5 characters long.")
+
+        if len(body) > 500:
+            raise forms.ValidationError("Comment not exceed 500 characters.")
+
+        return body
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic
-from .models import Post, Comment
-from django.http import HttpResponseRedirect
 from django.contrib import messages
-from .forms import CommentForm
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from django.views import generic
+
+from .forms import CommentForm
+from .models import Comment, Post
 
 
 class PostList(generic.ListView):
+    """List view for published posts."""
     model = Post
     template_name = "blog/index.html"
     paginate_by = 6
@@ -24,25 +27,18 @@ def post_detail(request, slug):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
-
-    #comments = post.comments.filter(approved=True).order_by("-created_on")
     comment_count = comments.count()
     comment_form = CommentForm()
 
     if request.method == "POST":
-        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.author = request.user
             comment.post = post
             comment.save()
-            messages.add_message(
-            request, messages.SUCCESS,
-            'Comment submitted and awaiting approval'
-    )
-            comment_form = CommentForm()  # Reset the form after saving
-            print("About to render template")
+            messages.success(request, "Comment submitted, awaiting approval")
+            return redirect("post_detail", slug=slug)
 
     return render(
         request,
@@ -55,18 +51,26 @@ def post_detail(request, slug):
         },
     )
 
+
 def profile_page(request):
-    user = get_object_or_404(User, user=request.user)
+    """
+    User profile page with their comments.
+    """
+    user = get_object_or_404(User, pk=request.user.id)
     comments = user.commenter.all()
+    return render(
+        request,
+        "blog/profile.html",
+        {"user": user, "comments": comments},
+    )
+
 
 def comment_edit(request, slug, comment_id):
     """
-    view to edit comments
+    View to edit comments.
     """
     if request.method == "POST":
-
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
+        post = get_object_or_404(Post, status=1, slug=slug)
         comment = get_object_or_404(Comment, pk=comment_id)
         comment_form = CommentForm(data=request.POST, instance=comment)
 
@@ -75,24 +79,24 @@ def comment_edit(request, slug, comment_id):
             comment.post = post
             comment.approved = False
             comment.save()
-            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+            messages.success(request, "Comment updated!")
         else:
-            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+            messages.error(request, "Error updating comment!")
 
-    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+    return HttpResponseRedirect(reverse("post_detail", args=[slug]))
+
 
 def comment_delete(request, slug, comment_id):
     """
-    view to delete comment
+    View to delete comments.
     """
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
+    post = get_object_or_404(Post, status=1, slug=slug)
     comment = get_object_or_404(Comment, pk=comment_id)
 
     if comment.author == request.user:
         comment.delete()
-        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+        messages.success(request, "Comment deleted!")
     else:
-        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+        messages.error(request, "You can only delete your own comments!")
 
-    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+    return HttpResponseRedirect(reverse("post_detail", args=[slug]))
